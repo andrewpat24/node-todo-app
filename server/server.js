604db69e89f9ced0986ae7d835c5c3836eab204d
Andrew Patterson <apat24@mail.sfsu.edu>
2017-03-02 02:25:40 -0800

    adds testing parameters for POST requests

--- server/server.js
+++ server/server.js
@@ -1,9 +1,44 @@
+// var express = require('express');
+// var bodyParser = require('body-parser');
+//
+// var {mongoose} = require('./db/mongoose');
+// var {Todo} = require('./models/todo');
+// var {User} = require('./models/user');
+//
+// var app = express();
+// var statusCode = -1;
+//
+// app.use(bodyParser.json());
+//
+// app.post('/todos', (req, res) => {
+//   var todo = new Todo({
+//     text: req.body.text
+//   });
+//
+//   todo.save().then((doc) => {
+//     res.status(200).send(doc);
+//     statusCode = 200;
+//   }, (e) => {
+//     res.status(400).send(e);
+//     statusCode = 400;
+//   });
+// });
+//
+//
+//
+//
+// app.listen(3000, () => {
+//   console.log(`Started on port 3000, the current status code is ${statusCode}`);
+// });
+//
+// module.exports = {app};
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
@@ -20,9 +55,10 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
-
-
+console.log();
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
