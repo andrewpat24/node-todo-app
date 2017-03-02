55f854bed522557ad78e9d42b9e0f3056056a1ea
Andrew Patterson <apat24@mail.sfsu.edu>
2017-03-02 02:57:08 -0800

    nothing to see here

--- server/server.js
+++ server/server.js
@@ -1,38 +1,3 @@
-// var express = require('express');
-// var bodyParser = require('body-parser');
-//
-// var {mongoose} = require('./db/mongoose');
-// var {Todo} = require('./models/todo');
-// var {User} = require('./models/user');
-//
-// var app = express();
-// var statusCode = -1;
-//
-// app.use(bodyParser.json());
-//
-// app.post('/todos', (req, res) => {
-//   var todo = new Todo({
-//     text: req.body.text
-//   });
-//
-//   todo.save().then((doc) => {
-//     res.status(200).send(doc);
-//     statusCode = 200;
-//   }, (e) => {
-//     res.status(400).send(e);
-//     statusCode = 400;
-//   });
-// });
-//
-//
-//
-//
-// app.listen(3000, () => {
-//   console.log(`Started on port 3000, the current status code is ${statusCode}`);
-// });
-//
-// module.exports = {app};
-
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -41,6 +6,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+var statusCode = -1;
 
 app.use(bodyParser.json());
 
@@ -51,21 +17,18 @@ app.post('/todos', (req, res) => {
 
   todo.save().then((doc) => {
     res.status(200).send(doc);
+    statusCode = 200;
   }, (e) => {
     res.status(400).send(e);
+    statusCode = 400;
   });
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({todos})
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
+
+
 
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+  console.log(`Started on port 3000`);
 });
 
 module.exports = {app};
